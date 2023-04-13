fd5dddf3fdad7d3158e83e92851c480fd6a5923e
Aneta <sopaaneta@gmail.com>
2023-04-13 09:33:01 +0200

    Added todo marked

--- index.js
+++ index.js
@@ -1,86 +1,100 @@
-// window.localStorage.getItem('name');
+console.log("test");
+const form = document.getElementById("list-form");
+const input = document.getElementById("list-form-input");
+const lists = document.getElementById("lists");
 
-
-window.addEventListener('load', () => {
-  const form = document.getElementById('new-task-form');
-  const input = document.getElementById('new-task-input');
-  const list = document.getElementById('tasks');
-
+window.addEventListener("load", () => {
   // Add action to form (create a tasks)
-  form.addEventListener('submit', (e) => {
-
+  form.addEventListener("submit", (e) => {
+    console.log(e);
     // No page reload
     e.preventDefault();
 
     // Value from input
-    const task = input.value;
+    const list = input.value;
 
-    if (!task) {
-      alert('Please fill out the task!');
+    if (!list) {
+      alert("Please fill out a to do!");
       return;
     }
 
     // Add task div
-    const task_el = document.createElement('div');
-    task_el.classList.add('task');
+    const to_do_el = document.createElement("div");
+    to_do_el.classList.add("list");
 
     // Add content div to task
-    const task_content_el = document.createElement('div');
-    task_content_el.classList.add('content');
-    task_el.appendChild(task_content_el);
+    const to_do_content_el = document.createElement("div");
+    to_do_content_el.classList.add("content");
+    to_do_el.appendChild(to_do_content_el);
 
     // Add input to content div
-    const task_input_el = document.createElement('input');
-    task_input_el.classList.add('text');
-    task_input_el.type = 'text';
-    task_input_el.value = task;
+    const to_do_input_el = document.createElement("input");
+    to_do_input_el.classList.add("text");
+    to_do_input_el.type = "text";
+    to_do_input_el.value = list;
     // 'readonly' - default input
-    task_input_el.setAttribute('readonly' , 'readonly');
-    task_content_el.appendChild(task_input_el);
+    to_do_input_el.setAttribute("readonly", "readonly");
+    to_do_content_el.appendChild(to_do_input_el);
 
     // Add action div of task
-    const task_action_el = document.createElement('div');
-    task_action_el.classList.add('actions');
+    const to_do_action_el = document.createElement("div");
+    to_do_action_el.classList.add("actions");
 
-    task_el.appendChild(task_action_el);
-    console.log(task_action_el)
+    to_do_el.appendChild(to_do_action_el);
 
     // Add button edit to action div
-    const task_button_edit_el = document.createElement('button');
-    task_button_edit_el.classList.add('edit');
-    task_button_edit_el.innerHTML = 'Edit';
+    const to_do_button_edit_el = document.createElement("button");
+    to_do_button_edit_el.classList.add("edit");
+    to_do_button_edit_el.innerHTML = "Edit";
 
     // Add button delete to action div
-    const task_button_delete_el = document.createElement('button');
-    task_button_delete_el.classList.add('delete');
-    task_button_delete_el.innerHTML = 'Delete';
+    const to_do_button_delete_el = document.createElement("button");
+    to_do_button_delete_el.classList.add("delete");
+    to_do_button_delete_el.innerHTML = "Delete";
 
-    task_action_el.appendChild(task_button_edit_el);
-    task_action_el.appendChild(task_button_delete_el);
+    to_do_action_el.appendChild(to_do_button_edit_el);
+    to_do_action_el.appendChild(to_do_button_delete_el);
 
     // Add task div to the list tasks
-    list.appendChild(task_el);
+    lists.appendChild(to_do_el);
 
-    input.value = '';
+    input.value = "";
 
-    task_button_edit_el.addEventListener('click', () => {
+    to_do_button_edit_el.addEventListener("click", () => {
+      e.preventDefault();
 
       // Check if button is edit
       // if yes - then change the text to Save and text change is possible
-      if (task_button_edit_el.innerText.toLowerCase() == 'edit') {
-        task_input_el.removeAttribute('readonly');
-        task_input_el.focus();
-        task_button_edit_el.innerText = 'Save';
+      if (to_do_button_edit_el.innerText.toLowerCase() === "edit") {
+        // Remove marked function
+        to_do_input_el.removeEventListener("click", markedToDo);
+        to_do_input_el.removeAttribute("readonly");
+        to_do_input_el.focus();
+        to_do_button_edit_el.innerText = "Save";
       } else {
-        task_input_el.setAttribute('readonly', 'readonly');
-        task_button_edit_el.innerText = 'edit';
+        to_do_input_el.setAttribute("readonly", "readonly");
+        to_do_button_edit_el.innerText = "edit";
+        to_do_input_el.addEventListener("click", markedToDo);
       }
     });
 
-    task_button_delete_el.addEventListener('click', () => {
-
+    to_do_button_delete_el.addEventListener("click", () => {
       // Remove the task
-      list.removeChild(task_el);
-    })
-  })
-})
+      lists.removeChild(to_do_el);
+    });
+
+    to_do_input_el.addEventListener("click", markedToDo);
+
+    function markedToDo() {
+      // When a to do is clicked, it will be marked
+      to_do_input_el.classList.add("marked");
+      to_do_action_el.removeChild(to_do_button_edit_el);
+    }
+
+    // to_do_input_el.addEventListener("click", function () {
+    //   // When a to do is clicked, it will be marked
+    //   to_do_input_el.classList.add("marked");
+    //   to_do_action_el.removeChild(to_do_button_edit_el);
+    // });
+  });
+});
